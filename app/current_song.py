1d1226b208cf6bc4f98a05c3cf4cd7a99d528bb8
Mitch Suzara <suzaram3@gmail.com>
2022-06-07 10:14:08 -0400

    refactor session.py and added get current song

---
+++ app/current_song.py
@@ -0,0 +1,27 @@
+from spotify import SpotifyHandler
+
+
+def parse_result(data: dict) -> dict:
+    return {
+        "song_id": data["item"]["id"],
+        "song_name": data["item"]["name"],
+        "artist_id": data["item"]["artists"][0]["id"],
+        "artist_id": data["item"]["artists"][0]["name"],
+        "album_id": data["item"]["album"]["id"],
+        "album_name": data["item"]["album"]["name"],
+        "album_release_year": data["item"]["album"]["release_date"][:4],
+        "spotify_url": data["item"]["external_urls"]["spotify"],
+    }
+
+
+def current_song() -> None:
+
+    spotify = SpotifyHandler()
+
+    raw_data = spotify.get_current_track()
+
+    print(f"CurrentSong:<{parse_result(raw_data)}>")
+
+
+if __name__ == "__main__":
+    current_song()
--- app/session.py
+++ app/session.py
@@ -1,10 +1,8 @@
-import datetime, json, logging, time
+import datetime, json, time
 
 from dataclasses import asdict
 from sqlalchemy.dialects.postgresql import insert as pg_insert
 from sqlalchemy import func
-
-logger = logging.getLogger("file")
 
 
 class SchemaEncoder(json.JSONEncoder):
@@ -31,10 +29,6 @@ class SessionHandler:
         SessionHandler.__instance = SessionHandler(session, model)
         return SessionHandler.__instance
 
-    def add(self, record_dict):
-        record_model = self.model(**record_dict)
-        self.session.add(record_model)
-
     def insert_many(self, record_list):
         statements = [
             pg_insert(self.model).values(record_dict).on_conflict_do_nothing()
@@ -42,47 +36,10 @@ class SessionHandler:
         ]
         return [self.session.execute(statement) for statement in statements]
 
-    def add_many(self, record_list):
-        return self.session.add_all(
-            [self.model(**record_dict) for record_dict in record_list]
-        )
-
     def update(self, query_dict, update_dict):
         return (
             self.session.query(self.model).filter_by(**query_dict).update(update_dict)
         )
-
-    def upsert(self, record_dict, set_dict, constraint):
-        statement = (
-            pg_insert(self.model)
-            .values(record_dict)
-            .on_conflict_do_update(constraint=constraint, set_=set_dict)
-        )
-        return self.session.execute(statement)
-
-    def get(self, id, to_json=None):
-        result = self.session.query(self.model).get(id)
-        return asdict(result) if to_json is None else self.to_json(result)
-
-    def get_one(self, query_dict, to_json=None):
-        result = self.session.query(self.model).filter_by(**query_dict).first()
-        return asdict(result) if to_json is None else self.to_json(result)
-
-    def get_latest(self, query_dict, to_json=None):
-        result = (
-            self.session.query(self.model)
-            .filter_by(**query_dict)
-            .order_by(self.model.updated_at.desc())
-            .first()
-        )
-        return (
-            None
-            if result is None
-            else (asdict(result) if to_json is None else self.to_json(result))
-        )
-
-    def get_count(self, query_dict, to_json=None):
-        return self.session.query(self.model).filter_by(**query_dict).count()
 
     def get_total_count(self):
         return self.session.query(self.model).count()
