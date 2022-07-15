085bfe9b0f64d0592fa3617c85f0181520637df8
Mitch Suzara <suzaram3@gmail.com>
2022-07-15 00:56:08 -0400

    so many changes! refactor config which cascaded to other files

--- app/current_song.py
+++ app/current_song.py
@@ -3,15 +3,19 @@ from spotify import SpotifyHandler
 
 spotify = SpotifyHandler()
 data = spotify.get_current_track()
-song_dict = {
-    "song_id": data["item"]["id"],
-    "song_name": data["item"]["name"],
-    "artist_id": data["item"]["artists"][0]["id"],
-    "artist_name": data["item"]["artists"][0]["name"],
-    "album_id": data["item"]["album"]["id"],
-    "album_name": data["item"]["album"]["name"],
-    "album_release_year": data["item"]["album"]["release_date"][:4],
-    "spotify_url": data["item"]["external_urls"]["spotify"],
-}
-print(f"CurrentSong:\n")
-[print(f"{k} : {v}") for k, v in song_dict.items()]
+if data:
+    song_dict = {
+        "song_id": data["item"]["id"],
+        "song_name": data["item"]["name"],
+        "artist_id": data["item"]["artists"][0]["id"],
+        "artist_name": data["item"]["artists"][0]["name"],
+        "album_id": data["item"]["album"]["id"],
+        "album_name": data["item"]["album"]["name"],
+        "album_release_year": data["item"]["album"]["release_date"][:4],
+        "spotify_url": data["item"]["external_urls"]["spotify"],
+    }
+    print(f"\nCurrent_Song:")
+    [print(f"{k} : {v}") for k, v in song_dict.items()]
+    print()
+else:
+    print("Current_Song: None")
---
+++ app/summary.py
@@ -0,0 +1,23 @@
+from sqlalchemy import cast, Date, func
+from qa_config import Config, Session
+
+c = Config()
+
+with Session() as session:
+    table_counts = [{key: session.query(c.models[key]).count()} for key in c.models]
+    stream_count_per_day = (
+        session.query(
+            func.count(cast(c.models["SongStreamed"].played_at, Date)),
+        )
+        .group_by(cast(c.models["SongStreamed"].played_at, Date))
+        .all()
+    )
+average_streams_per_day = sum([row[0] for row in stream_count_per_day]) // len(
+    stream_count_per_day
+)
+
+print(f"\n**SpotifyData**\n-Table Counts-")
+[print(f"{key}: {value:,}") for item in table_counts for key, value in item.items()]
+print(f"\n**Miscellaneous Data**")
+print(f"average_streams_per_day : {average_streams_per_day}")
+print()
