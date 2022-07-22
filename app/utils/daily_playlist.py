5c688251d7dc5694134df051b319bb56b5f52a00
Mitch Suzara <suzaram3@gmail.com>
2022-07-21 21:44:33 -0400

    updates to daily_playlist

--- app/utils/daily_playlist.py
+++ app/utils/daily_playlist.py
@@ -1,4 +1,3 @@
-from cgi import print_form
 from .spotify import SpotifyHandler
 from SpotifyHistory.config import Config
 from SpotifyHistory.app.utils.queries import yesterday_top_ten
@@ -7,3 +6,4 @@ c, sp = Config(), SpotifyHandler()
 data = yesterday_top_ten()
 sp.update_playlist(c.config["spotify"]["yesterday_top_10_id"], data["song_ids"])
 sp.update_playlist_details(c.config["spotify"]["yesterday_top_10_id"], data["desc"])
+c.file_logger.info(F"{c.config['spotify']['yesterday_top_10_id']} top ten playlist updated")
